17d27960a85754ad1ade3666e60ffd5fc562dcf8
bluesadi <liuyibo@stu.scu.edu.cn>
2021-11-09 06:26:07 -0800

    update

--- README.md
+++ README.md
@@ -14,34 +14,37 @@ fi
 cd ./Build
 cmake ../Transforms
 make
-cd ../Test
+```
+### Generate LLVM IR
+```
+clang -S -emit-llvm TestProgram.cpp -o IR/orig.ll
 ```
 ### Control Flow Flattening
 ```
-opt -lowerswitch -S IR/TestProgram_orig.ll -o IR/TestProgram_lowerswitch.ll
-opt -load ../Build/LLVMObfuscator.so -fla -split_num 3 -S IR/TestProgram_lowerswitch.ll -o IR/TestProgram_fla.ll
-clang IR/TestProgram_fla.ll -o Bin/TestProgram_fla
+opt -lowerswitch -S IR/orig.ll -o IR/lowerswitch.ll
+opt -load ../Build/LLVMObfuscator.so -fla -split_num 3 -S IR/lowerswitch.ll -o IR/fla.ll
+clang IR/fla.ll -o Bin/fla
 ```
 ### Bogus Control Flow
 ```
-opt -load ../Build/LLVMObfuscator.so -bcf -bcf_loop 1 -split_num 3 -S IR/TestProgram_orig.ll -o IR/TestProgram_bcf.ll
-clang IR/TestProgram_bcf.ll -o Bin/TestProgram_bcf
+opt -load ../Build/LLVMObfuscator.so -bcf -bcf_loop 1 -split_num 3 -S IR/orig.ll -o IR/bcf.ll
+clang IR/bcf.ll -o Bin/bcf
 ```
 ### Instruction Substitution
 ```
-opt -load ../Build/LLVMObfuscator.so -sub -sub_loop 2 -S IR/TestProgram_orig.ll -o IR/TestProgram_sub.ll
-clang IR/TestProgram_sub.ll -o Bin/TestProgram_sub
+opt -load ../Build/LLVMObfuscator.so -sub -sub_loop 2 -S IR/orig.ll -o IR/sub.ll
+clang IR/sub.ll -o Bin/sub
 ```
 ### Random Control Flow
 ```
-opt -load ../Build/LLVMObfuscator.so -rcf -S IR/TestProgram_orig.ll -o IR/TestProgram_rcf.ll
-llc -filetype=obj -mattr=+rdrnd IR/TestProgram_rcf.ll -o Bin/TestProgram_rcf.o
-clang Bin/TestProgram_rcf.o -o Bin/TestProgram_rcf
+opt -load ../Build/LLVMObfuscator.so -rcf -S IR/orig.ll -o IR/rcf.ll
+llc -filetype=obj -mattr=+rdrnd IR/rcf.ll -o Bin/rcf.o
+clang Bin/rcf.o -o Bin/rcf
 ```
 ### Constant Substitution
 ```
-opt -load ../Build/LLVMObfuscator.so -csub -csub_loop 2 -S IR/TestProgram_orig.ll -o IR/TestProgram_csub.ll
-clang IR/TestProgram_csub.ll -o Bin/TestProgram_csub
+opt -load ../Build/LLVMObfuscator.so -csub -csub_loop 2 -S IR/orig.ll -o IR/csub.ll
+clang IR/csub.ll -o Bin/csub
 ```
 ## Features
 ### Control Flow Flattening
--- test.sh
+++ test.sh
@@ -7,43 +7,43 @@ make
 cd ../Test
 
 # 未混淆的ELF文件
-clang -S -emit-llvm TestProgram.cpp -o IR/TestProgram_orig.ll
-clang IR/TestProgram_orig.ll -o Bin/TestProgram_orig
+clang -S -emit-llvm TestProgram.cpp -o IR/orig.ll
+clang IR/orig.ll -o Bin/orig
 echo -e "\n\033[32m> Test case on original binary <\033[0m"
-./Bin/TestProgram_orig flag{s1mpl3_11vm_d3m0}
+./Bin/orig flag{s1mpl3_11vm_d3m0}
 
 # 基本块分割
-opt -load ../Build/LLVMObfuscator.so -split -split_num 3 -S IR/TestProgram_orig.ll -o IR/TestProgram_split.ll
-clang IR/TestProgram_split.ll -o Bin/TestProgram_split
+opt -load ../Build/LLVMObfuscator.so -split -split_num 3 -S IR/orig.ll -o IR/split.ll
+clang IR/split.ll -o Bin/split
 
 # 控制流平坦化
-opt -lowerswitch -S IR/TestProgram_orig.ll -o IR/TestProgram_lowerswitch.ll
-opt -load ../Build/LLVMObfuscator.so -fla -split_num 3 -S IR/TestProgram_lowerswitch.ll -o IR/TestProgram_fla.ll
-clang IR/TestProgram_fla.ll -o Bin/TestProgram_fla
+opt -lowerswitch -S IR/orig.ll -o IR/lowerswitch.ll
+opt -load ../Build/LLVMObfuscator.so -fla -split_num 3 -S IR/lowerswitch.ll -o IR/fla.ll
+clang IR/fla.ll -o Bin/fla
 echo -e "\n\033[32m> Test case on Flattening <\033[0m"
-./Bin/TestProgram_fla flag{s1mpl3_11vm_d3m0}
+./Bin/fla flag{s1mpl3_11vm_d3m0}
 
 # 虚假控制流
-opt -load ../Build/LLVMObfuscator.so -bcf -bcf_loop 1 -split_num 3 -S IR/TestProgram_orig.ll -o IR/TestProgram_bcf.ll
-clang IR/TestProgram_bcf.ll -o Bin/TestProgram_bcf
+opt -load ../Build/LLVMObfuscator.so -bcf -bcf_loop 1 -split_num 3 -S IR/orig.ll -o IR/bcf.ll
+clang IR/bcf.ll -o Bin/bcf
 echo -e "\n\033[32m> Test case on BogusControlFlow <\033[0m"
-./Bin/TestProgram_bcf flag{s1mpl3_11vm_d3m0}
+./Bin/bcf flag{s1mpl3_11vm_d3m0}
 
 # 指令替换
-opt -load ../Build/LLVMObfuscator.so -sub -sub_loop 2 -S IR/TestProgram_orig.ll -o IR/TestProgram_sub.ll
-clang IR/TestProgram_sub.ll -o Bin/TestProgram_sub
+opt -load ../Build/LLVMObfuscator.so -sub -sub_loop 2 -S IR/orig.ll -o IR/sub.ll
+clang IR/sub.ll -o Bin/sub
 echo -e "\n\033[32m> Test case on Substitution <\033[0m"
-./Bin/TestProgram_sub flag{s1mpl3_11vm_d3m0}
+./Bin/sub flag{s1mpl3_11vm_d3m0}
 
 # 随机控制流
-opt -load ../Build/LLVMObfuscator.so -rcf -S IR/TestProgram_orig.ll -o IR/TestProgram_rcf.ll
-llc -filetype=obj -mattr=+rdrnd IR/TestProgram_rcf.ll -o Bin/TestProgram_rcf.o
-clang Bin/TestProgram_rcf.o -o Bin/TestProgram_rcf
+opt -load ../Build/LLVMObfuscator.so -rcf -S IR/orig.ll -o IR/rcf.ll
+llc -filetype=obj -mattr=+rdrnd IR/rcf.ll -o Bin/rcf.o
+clang Bin/rcf.o -o Bin/rcf
 echo -e "\n\033[32m> Test case on RandomControlFlow <\033[0m"
-./Bin/TestProgram_rcf flag{s1mpl3_11vm_d3m0}
+./Bin/rcf flag{s1mpl3_11vm_d3m0}
 
 # 常量替换
-opt -load ../Build/LLVMObfuscator.so -csub -csub_loop 2 -S IR/TestProgram_orig.ll -o IR/TestProgram_csub.ll
-clang IR/TestProgram_csub.ll -o Bin/TestProgram_csub
+opt -load ../Build/LLVMObfuscator.so -csub -csub_loop 2 -S IR/orig.ll -o IR/csub.ll
+clang IR/csub.ll -o Bin/csub
 echo -e "\n\033[32m> Test case on ConstantSubstitution <\033[0m"
-./Bin/TestProgram_csub flag{s1mpl3_11vm_d3m0}
+./Bin/csub flag{s1mpl3_11vm_d3m0}
