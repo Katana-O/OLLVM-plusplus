64a771a9f74d69817d709fa2eee60683c397ae14
bluesadi <liuyibo@stu.scu.edu.cn>
2021-11-29 17:24:34 -0800

    Update TestPrograms. Add AES tests

--- Test/TestProgram-1.cpp
+++ Test/TestProgram-1.cpp
@@ -1,10 +1,10 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include "AES.h"
 
 char *input;
-char enc[100] = "\x86\x8a\x7d\x87\x93\x8b\x4d\x81\x80\x8a\
-\x43\x7f\x49\x49\x86\x71\x7f\x62\x53\x69\x28\x9d"; 
+char enc[100] = "\xce\x6a\x64\x09\x74\x00\xf7\xf0\x86\x31\x5b\x78\xe3\x05\x6f\xfa\xcf\x31\x00\x45\xff\x19\xdf\xeb\xea\xcc\x33\x97\x26\xce\xbc\xa3"; 
 void encrypt(unsigned char *dest, char *src){
     int len = strlen(src);
     for(int i = 0;i < len;i ++){
@@ -21,9 +21,10 @@ int main(int argc, char *argv[]){
     }
     input = argv[1];
     printf("Your flag is: %s\n", input);
-    unsigned char dest[100] = {0}; 
-    encrypt(dest, input); 
-    bool result = strlen(input) == 22 && !memcmp(dest, enc, 22); 
+    AES aes((BYTE*)"1234567812345678", 128);
+    int outlen;
+    unsigned char *dest = aes.EncryptECB((BYTE*)"flag{s1mpl3_11vm_d3m0}", 22, outlen);
+    bool result = strlen(input) == 22 && !memcmp(dest, enc, 32); 
     if(result){ 
         printf("Congratulations~\n"); 
     }else{
--- test.sh
+++ test.sh
@@ -4,25 +4,28 @@ cd Build
 cmake ../Transforms
 make
 cd ../Test
-mkdir -p Bin
-mkdir -p IR
+rm -rf Bin IR
+mkdir -p Bin IR
 echo -e "\n\033[32m[*] Compilation completed \033[0m"
 
 runTest(){
     echo -e "\n\033[32m[*] Test $1 \033[0m"
     opt -load ../Build/LLVMObfuscator.so -$2 -split_num 3 -S IR/orig.ll -o IR/$2.ll
+    opt -load ../Build/LLVMObfuscator.so -$2 -split_num 3 -S IR/AES.ll -o IR/AES_$2.ll
     if test $2 = "rcf"
     then
         llc -filetype=obj -mattr=+rdrnd IR/rcf.ll -o Bin/rcf.o
-        clang Bin/rcf.o -o Bin/rcf
+        llc -filetype=obj -mattr=+rdrnd IR/AES_rcf.ll -o Bin/AES_rcf.o
+        clang++ Bin/rcf.o Bin/AES_$2.o -o Bin/rcf
     else
-        clang IR/$2.ll -o Bin/$2
+        clang++ IR/$2.ll IR/AES.ll -o Bin/$2
     fi
     ./Bin/$2 flag{s1mpl3_11vm_d3m0}
 }
 
 # 编译未混淆的 LLVM IR 文件
-clang -S -emit-llvm TestProgram-1.cpp -o IR/orig.ll
+clang++ -S -emit-llvm TestProgram-1.cpp -o IR/orig.ll
+clang++ -S -emit-llvm AES.cpp -o IR/AES.ll
 
 # 控制流平坦化
 runTest "Flattening" "fla"
